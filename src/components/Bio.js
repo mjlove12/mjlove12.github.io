3977a24ed64658ad2946cbc829dca845b91252b9
Matthew Love <mjlove12@gmail.com>
2018-09-06 20:52:55 -0400

    initial commit with bare site

---
+++ src/components/Bio.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+          textAlign: 'center',
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Kyle Mathews`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+            borderRadius: '50%',
+          }}
+        />
+        <br/>
+        <p>
+          Sharing our small <strong>adventures</strong> home and abroad.{' '}
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
---
+++ src/layouts/index.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Bio from '../components/Bio'
+
+import { rhythm, scale } from '../utils/typography'
+
+class Template extends React.Component {
+  render() {
+    const { location, children } = this.props
+    let header
+
+    let rootPath = `/`
+    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
+      rootPath = __PATH_PREFIX__ + `/`
+    }
+
+    if (location.pathname === rootPath) {
+      header = (
+        <h1
+          style={{
+            ...scale(1.5),
+            marginBottom: rhythm(1.5),
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: 'none',
+              textDecoration: 'none',
+              color: 'inherit',
+            }}
+            to={'/'}
+          >
+            Gatsby Starter Blog
+          </Link>
+        </h1>
+      )
+    } else {
+      header = (
+        <h3
+          style={{
+            fontFamily: 'Montserrat, sans-serif',
+            marginTop: 0,
+            marginBottom: rhythm(-1),
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: 'none',
+              textDecoration: 'none',
+              color: 'inherit',
+            }}
+            to={'/'}
+          >
+            Gatsby Starter Blog
+          </Link>
+        </h3>
+      )
+    }
+    return (
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div
+        style={{
+          height: '100%',
+          width: '20%',
+          position: 'fixed',
+          zIndex: 1,
+          top: 0,
+          left: 0,
+          // backgroundColor: 'lightgrey',
+          overflowX: 'hidden', // disable horizontal scroll
+          paddingTop: '20px',
+          borderRight: '1px solid lightgrey',
+          margin: 10,
+        }}
+      >
+        <Bio/>
+      </div>
+      <div
+        style={{
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        {header}
+        {children()}
+      </div>
+      </div>
+    )
+  }
+}
+
+export default Template
