2c810f31debc8b18ac98c0356ecbd72d9db11709
Matthew Love <mjlove12@gmail.com>
2018-09-06 21:14:54 -0400

    adding CNAME

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
   },
-  pathPrefix: '/mjlove12.github.io',
+  // pathPrefix: '/mjlove12.github.io',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
